20cdc4004daa2f5981bb3d07723ee98511fe4f89
Roarw <8555390+Roarw@users.noreply.github.com>
2023-04-23 10:20:46 +0200

    Model first

---
+++ PlaygroundNetAPI/Models/RawIngredient.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlaygroundNetAPI.Models
+{
+    public class RawIngredient
+    {
+        public int ID { get; set; }
+        public string? GUID { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Price { get; set; }
+        public DateTime Updated { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string? CreatedBy { get; set; }
+    }
+}
