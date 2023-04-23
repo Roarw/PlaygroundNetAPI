857247e595aeb86c8aa6cfa3757641a3141a1945
Roarw <8555390+Roarw@users.noreply.github.com>
2023-04-23 10:30:00 +0200

    Model second

---
+++ PlaygroundNetAPI/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿namespace PlaygroundNetAPI.Models
+{
+    public class Recipe
+    {
+        public int ID { get; set; }
+        public string? GUID { get; set; }
+        public string? Name { get; set; }
+        public List<RawIngredient>? Ingredients { get; set; }
+        public string? Description { get; set; }
+        public DateTime Updated { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string? CreatedBy { get; set; }
+    }
+}
